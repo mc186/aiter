67a83cfba432df4c743e678f0499ff17f6fad4b0
Tianxing Wu <tianxing.wu@amd.com>
2025-06-17 09:00:14 +0000

    linter

--- aiter/ops/triton/gemm_afp4wfp4.py
+++ aiter/ops/triton/gemm_afp4wfp4.py
@@ -8,7 +8,10 @@ import os
 import torch
 import triton
 import triton.language as tl
-from aiter.ops.triton.utils.pid_preprocessing import pid_grid, remap_xcd_chunked, remap_xcd
+from aiter.ops.triton.utils.pid_preprocessing import (
+    pid_grid,
+    remap_xcd_chunked,
+)
 import aiter.ops.triton.utils.arch_info as arch_info
 from aiter.ops.triton.utils.core import AITER_TRITON_CONFIGS_PATH
 
