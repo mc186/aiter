5534a9a14333710028789ee73f591b23c6ba90b3
ShaoChunLee <Shao-Chun.Lee@amd.com>
2025-06-26 21:51:55 +0000

    tune fp4 prequant gemm atomic

--- op_tests/triton_tests/test_gemm_afp4wfp4_pre_quant_atomic.py
+++ op_tests/triton_tests/test_gemm_afp4wfp4_pre_quant_atomic.py
@@ -71,7 +71,7 @@ def get_x_vals():
     ]
     x_vals += [(2 ** (v - 1), 4096 * v, 4096 * v) for v in range(1, 6)]
     x_vals += [(16, 16384, 3328 * 2), (128, 16384, 3328 * 2)]
-    x_vals += [(32, 512, 7168)]
+    x_vals += [(2**v, 512, 7168) for v in range(10)]
     return x_vals
 
 
