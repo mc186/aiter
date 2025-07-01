1981efda948c6b9e0fcedfce7cfdd8a8c32c55ea
valarLip <340077269@qq.com>
2025-07-01 06:13:43 +0000

    merge mxfp4 ut

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -11,7 +11,7 @@ from aiter import ActivationType, QuantType, dtypes
 from aiter.utility import fp4_utils
 
 # from aiter import get_torch_quant as get_quant
-from aiter import get_triton_quant as get_quant
+from aiter import get_hip_quant as get_quant
 from aiter.utility.fp4_utils import moe_mxfp4_sort
 from aiter.jit.core import AITER_ROOT_DIR, PY, get_asm_dir, bd_dir, mp_lock
 from aiter.jit.utils.chip_info import get_cu_num
