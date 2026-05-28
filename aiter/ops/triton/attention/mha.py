2c13d92ca8f83e1069147df1c9bb333557a9b0fd
manschou <manschou@amd.com>
2026-05-28 19:32:17 +0000

    Fix: Add missing SWIZZLE parameter to kernel call

--- aiter/ops/triton/attention/mha.py
+++ aiter/ops/triton/attention/mha.py
@@ -303,6 +303,7 @@ def _flash_attn_forward(
             VARLEN=is_varlen,
             BATCH=batch,
             NUM_XCD=get_num_xcds(),
+            SWIZZLE=int(os.environ.get('AITER_SWIZZLE', '0')),
             USE_INT64_STRIDES=_USE_INT64_STRIDES,
             ENABLE_SINK=sink is not None,
             SLIDING_WINDOW=sliding_window,
